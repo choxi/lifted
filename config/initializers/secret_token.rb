6fe737c0892652f6ec13fb2ad92deff8856ed62c
Roshan Choxi <roshan.choxi@gmail.com>
2013-12-25 18:20:04 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lifted::Application.config.secret_key_base = '94ea6f047cc204668ae7497435b688978c6beb96c3f8cb51375dc9672112c00d49adc7a495896822b430f339a706b9321b26166ee394292ce84574fed9dcab8c'
